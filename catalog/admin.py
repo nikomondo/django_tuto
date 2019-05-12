b5aca1428a698df5df698367317a8cde17129216
nicolas <nicobastien@free.fr>
2019-05-12 19:31:18 +0000

    models

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
 # Register your models here.
+from catalog.models import Author, Genre, Book, BookInstance
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin): 
+    pass
+    
+
+admin.site.register(Book)
+admin.site.register(Author)
+admin.site.register(Genre)
+admin.site.register(BookInstance)
